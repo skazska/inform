db6adc40e3365e3fddf43b3fea59915bc1546a61
skazska <skazska@gmail.com>
2018-10-05 08:47:35 +0500

    add promise to inform, close #3

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const logUpdate = require('log-update');
 
 const Group = require('./group');
 
+// TODO tests for Inform, separate output, and make definable through options, exclude log-update
+
 class Inform {
     constructor (text) {
         this.groups = [];
         this.addGroup({text: text});
+        this._promise = new Promise((resolve) => {
+            this._complete = () => {
+                resolve();
+            }
+        });
     }
 
     /**
@@ -26,8 +33,26 @@ class Inform {
         group.on('change', (event) => {
             this.render();
         });
+        group.on('end', () => {
+            this._checkEnd();
+        });
         this.groups.push(group);
         return group;
+    }
+
+    get promise () {
+        return this._promise;
+    }
+
+    /**
+     * checks if all informers are done then sets staus 3 and fire 'end'
+     * @private
+     */
+    _checkEnd () {
+        if (this.groups.every(informer => informer.isDone)) {
+            this._status = 3;
+            this._complete();
+        }
     }
 
     renderText () {
